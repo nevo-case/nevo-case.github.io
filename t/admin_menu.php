b328a352d7d28657770ff10ddcb7da4be7ecef1a
nevo-case <39429290+nevo-case@users.noreply.github.com>
2018-05-19 11:18:16 +0300

    Revert "easydrop"
    This reverts commit 74ac2c6ac4ef0c464e45fc989d8ba2c55b51ea2c.

--- t/admin_menu.php
+++
@@ -1,5 +0,0 @@
-                    <li><a href="/admin.php?case">Кейсы + Рандом</a></li>
-					<li><a href="/admin.php?user">Пользователи</a></li>
-                    <li><a href="/admin.php?design">Дизайн</a></li>
-					<li><a href="/admin.php?settings">Настройки</a></li>
-					<li><a href="/admin.php?logs">Выигрыши</a></li>
